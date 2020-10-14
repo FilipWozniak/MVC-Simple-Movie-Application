8dd0727f30e6e9b5691315f3fc46d0819c66a1c6
Filip Wozniak <filip@Mac.local>
2020-10-14 08:45:48 +0200

    Initial Commit

---
+++ MVC_Movie/Data/MVC_MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Movie.Models
+{
+    public class MVC_MovieContext : DbContext
+    {
+        public MVC_MovieContext (DbContextOptions<MVC_MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Movie.Models.Movie> Movie { get; set; }
+    }
+}
